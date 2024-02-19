6aa12bd17662a339d2b3ef55319087f7387f89fa
Mobina.Ghasempour <romi.ghasem@gmail.com>
2024-02-19 18:17:16 +0330

    two similar products rendered in page.

--- src/fetchData.js
+++ src/fetchData.js
@@ -1,16 +1,22 @@
 
 const fetchData = () => {
-  const product_name = document.querySelector('.product-name');
-  const product_cost = document.querySelector('.product-cost');
-  const product_description = document.querySelector('.product-description');
+  const product_name = document.getElementsByClassName('product-name');
+  // console.log(product_name);
+  const product_cost = document.querySelectorAll('.product-cost');
+  const product_description = document.querySelectorAll('.product-description');
   const getData = async () => {
     const data = await fetch('../src/data/data.json').then((response) => response.json());
     // console.log(data.products);
-    data.products.map(({name, des, cost }) => {
-      product_name.innerText = name;
-      product_cost.innerText = cost;
-      product_description.innerText = des;
+    data.products.map(({name, cost, des}) => {
+      for (let p_name of product_name) {
+        p_name.innerText = name;
+      }
+      for (let i = 0; i < 2; i++) {
+        product_cost[i].innerText = cost;
+        product_description[i].innerText = des;
+      }
     })
+    
   };
   getData();
   
