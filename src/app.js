6b8fd1d88a64a3b076d5f667a085cbb91a518afd
Mobina.Ghasempour <romi.ghasem@gmail.com>
2024-03-01 16:14:18 +0330

    searchInput added before container.

--- src/app.js
+++ src/app.js
@@ -2,11 +2,13 @@ import products from './productsConstructure.js';
 import fetchData from './fetchData.js';
 import shoppingCartList from './shoppingCartList.js';
 import calculate from './calculate.js';
+import searchProduct from './searchProduct.js';
 
 export const root = document.querySelector('.root');
 export const container = document.createElement('div');
 container.className = 'container';
 
+searchProduct();
 root.append(container);
 products();
 // products();
--- src/searchProduct.js
+++ src/searchProduct.js
@@ -1,1 +1,10 @@
+import { root } from "./app.js";
 
+const searchProduct = () => {
+
+  const header = document.createElement('div');
+  root.append(header);
+  const searchInput = document.createElement('input');
+  header.append(searchInput);
+ }
+export default searchProduct;
